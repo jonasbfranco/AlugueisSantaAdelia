86c745c15239c6d56e2077118146d2ec21e78702
Jonas B Franco <jonasbfranco@hotmail.com>
2023-03-10 21:46:30 -0300

    :fire: Final Project

--- backend/controllers/partyController.js
+++ backend/controllers/partyController.js
@@ -42,6 +42,86 @@ const partyController = {
         }
     },
 
+    getAll: async (req, res) => {
+        try {
+            const parties = await PartyModel.find();
+
+            res.status(200).json({parties, msg: 'Festas encontradas com sucesso!'});
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    getById: async(req, res) => {
+        try {
+            const id = req.params.id;
+            const party = await PartyModel.findById(id);
+
+            if(!party) {
+                res.status(404).json({msg: 'Festa não encontrada.'});
+                return;
+            }
+            
+            res.status(200).json({party, msg: 'Festa encontrada com sucesso!'});
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    delete: async(req, res) => {
+        try {
+            const id = req.params.id;
+            const party = await PartyModel.findById(id);
+
+            if(!party) {
+                res.status(404).json({msg: 'Festa não encontrada.'});
+                return;
+            }
+
+            const deleteParty = await PartyModel.findByIdAndDelete(id);
+
+            res.status(200).json({deleteParty, msg: 'Festa excluída com sucesso'});
+
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    update: async(req, res) => {
+        try {
+            const id = req.params.id;
+
+            const party = {
+                title: req.body.title,
+                author: req.body.author,
+                description: req.body.description,
+                budget: req.body.budget,
+                image: req.body.image,
+                services: req.body.services,
+            };
+
+
+            // BUDGET < VALOR DOS SERVIÇOS != NOVO SERVICO
+            if (party.services && !checkPartyBudget(party.budget, party.services)) {
+                res.status(406).json({msg: "O seu orçamento é insuficiente."});
+                return;
+            }
+
+            const updateParty = await PartyModel.findByIdAndUpdate(id, party);
+
+            if(!updateParty) {
+                res.status(404).json({msg: 'Festa não encontrada.'});
+                return;
+            }
+
+            res.status(200).json({party, msg: 'Festa atualizada com sucesso'});
+
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
 
 };
 
